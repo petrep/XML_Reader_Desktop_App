0f7671337120fa69179758a6b666ce26c1b7bc9c
petrep <petrep@freemail.hu>
2017-09-03 19:52:55 +0200

    Started to write 4 new classes to read/write XML documents

---
+++ src/com/test/DOM_Parser.java
@@ -0,0 +1,12 @@
+//https://stackoverflow.com/questions/5059224/which-is-the-best-library-for-xml-parsing-in-java
+
+package com.test;
+
+public class DOM_Parser {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ src/com/test/StAX_ReaderWriter_SAXParserFactory.java
@@ -0,0 +1,9 @@
+package com.test;
+
+public class StAX_ReaderWriter_SAXParserFactory {
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
